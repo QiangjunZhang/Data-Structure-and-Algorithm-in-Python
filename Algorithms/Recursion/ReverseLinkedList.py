9cbbfe9f1922c53d28d177a3516f49380ab0c247
QiangjunZhang <zhangqj616@gmail.com>
2020-08-11 12:09:46 +0100

    update

---
+++ Algorithms/Recursion/ReverseLinkedList.py
@@ -0,0 +1,28 @@
+# Recursion
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class SolutionRec:
+    def reverseList(self, head):
+        return self._reverse(head)
+
+    def _reverse(self, curr, prev=None):
+        if not curr:
+            return prev
+        after = curr.next
+        curr.next = prev
+        return self._reverse(after, curr)
+
+
+# Iteration
+class SolutionIte:
+    def reverseList(self, head):
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
