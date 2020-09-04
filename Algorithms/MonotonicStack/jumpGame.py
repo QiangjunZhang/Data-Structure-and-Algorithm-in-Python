68f97342784f411a207ad016270b8d2b0aa9317e
qiangjun <zhangqj616@gmail.com>
2020-09-04 21:11:07 +0100

    add a monotonic stack algorithm example problem and testing cases

---
+++ Algorithms/MonotonicStack/jumpGame.py
@@ -0,0 +1,17 @@
+class JumpGame:
+    def jump(self, nums: List[int]) -> int:
+        """
+        keep a monotonic jump range
+        """
+        stack = [0] * len(nums)
+        for i in range(len(nums) - 2, -1, -1):
+            if i + nums[i] >= len(nums) - 1:
+                stack[i] = 1
+            elif nums[i] < 1:
+                stack[i] = float('inf')
+            else:
+                stack[i] = stack[i + nums[i]] + 1
+            while stack[i] < stack[i + 1] and i < len(nums) - 1:
+                stack[i + 1] = stack[i]
+                i += 1
+        return stack[0]
